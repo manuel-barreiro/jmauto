24bf9fe815342bb8112ab34785e80d2c6ce14bb0
Manuel <manuel.barreiro@ing.austral.edu.ar>
2024-01-26 16:31:39 +1100

    Add VisitUs component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/Hero";
 import CardsSection from "@/components/CardsSection";
 import Footer from "@/components/Footer";
 import WhoAreWe from "@/components/WhoAreWe";
+import VisitUs from "@/components/VisitUs";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <Hero />
       <CardsSection />
       <WhoAreWe/>
+      <VisitUs />
       <Footer />
     </>
   );

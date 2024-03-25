3e5815afc8e241160b26cd607f72397c2de5ac9c
Manuel <manuel.barreiro@ing.austral.edu.ar>
2024-03-25 09:58:39 -0300

    Update title in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Z&Z Automóviles",
+  title: "Z&Z Automotores",
   description: "Compramos tu auto en tiempo récord",
 };
 
